3554eb6a043a338edf6abba4ead8d258abfb0f1a
FreakyCoder <kurayogun@gmail.com>
2022-08-16 19:46:20 +0300

    feat: profile user example for zustand usage

--- src/screens/profile/ProfileScreen.style.ts
+++ src/screens/profile/ProfileScreen.style.ts
@@ -3,6 +3,8 @@ import { ViewStyle, StyleSheet } from "react-native";
 
 interface Style {
   container: ViewStyle;
+  userContainer: ViewStyle;
+  userButton: ViewStyle;
 }
 
 export default (theme: ExtendedTheme) => {
@@ -13,6 +15,20 @@ export default (theme: ExtendedTheme) => {
       backgroundColor: colors.background,
       alignItems: "center",
       justifyContent: "center",
+    },
+    userContainer: {
+      marginTop: 16,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    userButton: {
+      height: 35,
+      width: 150,
+      marginTop: 32,
+      borderRadius: 16,
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: "#135",
     },
   });
 };
--- src/screens/profile/ProfileScreen.tsx
+++ src/screens/profile/ProfileScreen.tsx
@@ -6,6 +6,8 @@ import { useTheme } from "@react-navigation/native";
  */
 import createStyles from "./ProfileScreen.style";
 import Text from "@shared-components/text-wrapper/TextWrapper";
+import useStore from "@services/zustand/store";
+import RNBounceable from "@freakycoder/react-native-bounceable";
 
 interface ProfileScreenProps {}
 
@@ -14,11 +16,26 @@ const ProfileScreen: React.FC<ProfileScreenProps> = () => {
   const { colors } = theme;
   const styles = useMemo(() => createStyles(theme), [theme]);
 
+  const userData = useStore((state) => state.userData);
+  const setUserData = useStore((state) => state.setUserData);
+
   return (
     <View style={styles.container}>
       <Text h1 color={colors.text}>
         Profile
       </Text>
+      <View style={styles.userContainer}>
+        <Text>{userData?.name}</Text>
+        <Text>{userData?.email}</Text>
+      </View>
+      <RNBounceable
+        style={styles.userButton}
+        onPress={() => {
+          setUserData({ name: "John Doe", email: "johndoe@gmail.com" });
+        }}
+      >
+        <Text color="#fff">Set User</Text>
+      </RNBounceable>
     </View>
   );
 };
