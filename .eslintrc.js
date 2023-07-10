aaa45a2d213d245cf50f5271ffa4ada51b4fd025
FreakyCoder <WrathChaos@users.noreply.github.com>
2023-07-10 22:18:35 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
-    "@react-native-community",
     "prettier",
   ],
   ignorePatterns: [
