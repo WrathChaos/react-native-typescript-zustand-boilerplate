b35a1c9c40d3d643cdabc7054ef45c21437a1b55
FreakyCoder <kurayogun@gmail.com>
2023-07-29 17:42:48 +0300

    feat: better localization usage

--- src/shared/localization/index.ts
+++ src/shared/localization/index.ts
@@ -1,26 +1,8 @@
 import LocalizedStrings from "react-native-localization";
 
-export const localStrings = new LocalizedStrings({
-  "tr-TR": {
-    noLocationService: "Lokasyon Servisi Kapalı",
-    logout: "Çıkış",
-    yes: "Evet",
-    no: "Hayır",
-    cancel: "İptal",
-    areYouSure: "Emin misiniz?",
-    logoutDesc: "Çıkış yapmak üzeresiniz onaylıyor musunuz?",
-    noInternet: "İnternet Bağlantısı Yok",
-  },
-  en: {
-    noLocationService: "Location Service Not Available",
-    logout: "Logout",
-    yes: "Yes",
-    no: "No",
-    cancel: "Cancel",
-    areYouSure: "Are you sure?",
-    logoutDesc: "You are about to log out, confirm?",
-    noInternet: "No Internet Connection",
-  },
+import trTranslations from "./tr.localization";
+import enTranslations from "./en.localization";
+export const translations = new LocalizedStrings({
+  en: enTranslations,
+  tr: trTranslations,
 });
-// ? Set the language manually
-localStrings.setLanguage("tr");
