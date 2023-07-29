9047b745e744366f755860aab7ba048159068f3a
FreakyCoder <WrathChaos@users.noreply.github.com>
2023-07-29 21:21:28 +0300

    Update navigation.d.ts

--- navigation.d.ts
+++ navigation.d.ts
@@ -1,10 +1,16 @@
 declare module "@react-navigation/native" {
+  import { NavigationContainerProps } from "react-navigation";
+
   export type ThemeColors = {
-    [key: string]: string;
+    [key: string]: any;
   };
 
   export interface Theme {
     dark: boolean;
     colors: ThemeColors;
   }
+
+  export function NavigationContainer(
+    props: NavigationContainerProps<Theme>, // Ensure the correct type is used here
+  ): React.ReactElement;
 }
